d801aacf386a171958807de2f633cd7326e2bd74
Jonathan Wakely <cxx@kayari.org>
2025-11-06 03:20:45 +0000

    Add new_resolution.sh script

---
+++ bin/new_resolution.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+usage() { echo "Usage: $0 <issue number>" ; exit $1 ; }
+
+test $# -eq 1 || usage 1 >&2
+test "$1" == -h -o "$1" == --help && usage
+
+(( "$1" )) 2>/dev/null && test -f xml/issue$1.xml || {
+  echo "$0: Invalid issue number: $1"
+  exit 1
+} >&2
+
+issue=xml/issue$1.xml
+
+date=$(date +%Y-%m-%d)
+
+sed -i -e '/<\/discussion>/i\
+<superseded>' -e '/<\/discussion>/,/^<resolution>/d' -e '/^<\/resolution>/i\
+</superseded>\
+\
+<note>'$date'; <<AUTHOR>> provides improved wording</note>\
+\
+</discussion>\
+\
+<resolution>' $issue
