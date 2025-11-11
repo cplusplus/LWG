0ff30030c5cefdc6cc4ec1165cde81501c6c6b78
Jonathan Wakely <cxx@kayari.org>
2025-11-11 10:47:48 +0000

    Improve sweep_voting_to_wp.sh script

--- bin/sweep_voting_to_wp.sh
+++ bin/sweep_voting_to_wp.sh
@@ -2,7 +2,7 @@
 
 # Sweep all issues from Voting -> WP
 
-if [ -z "$1" ]
+if [ $# -ne 2 ]
 then
   echo "Usage: $0 '<Meeting Name> YYYY-MM-DD'" >&2
   exit 1
@@ -12,9 +12,7 @@ today=$(date +%F)
 meeting=$1
 date=$2
 
-# We could use awk -F'"' '/status="voting"/ { print $2}' xml/issue*.xml here,
-# but the wildcard matches thousands of files, so let git grep loop over them.
-git grep 'status="Voting"' xml | awk -F'"' '{print $2}' | while read inum
+bin/list_issues Voting | while read inum
 do
   bin/set_status $inum WP
   sed -i "s/^<note>$today /<note>$meeting $date; /" xml/issue$inum.xml
