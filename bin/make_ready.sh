3527ecb685be497d0d3e54999835c738b741b1d2
Jonathan Wakely <cxx@kayari.org>
2026-06-15 18:18:19 +0100

    Add "die" function to make_ready.sh script

--- bin/make_ready.sh
+++ bin/make_ready.sh
@@ -3,6 +3,11 @@
 # Script to change an active issue to Ready and commit the change.
 # Usage: make_ready.sh <ISSUE NUM> [MEETING NAME]
 
+die() {
+  echo "$0: $*" >&2
+  exit 1
+}
+
 issue=$1
 xml=xml/issue$issue.xml
 meeting=${2:+--prefix="$2"}
@@ -10,7 +15,7 @@ meeting=${2:+--prefix="$2"}
 old_status=$(xpath -q -e 'string(/issue/@status)' $xml)
 case "$old_status" in
   New|Open|LEWG|SG1|SG6) ;;
-  *) echo "Issue does not have an active status: $old_status" 2>&1; exit 1 ;;
+  *) die "Issue $issue does not have an active status: $old_status" ;;
 esac
 bin/set_status $issue Ready
 bin/add_note.sh $issue $meeting "Change status: $old_status &rarr; Ready."
